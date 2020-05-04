e4e1e8508cc32cb7ef506de563730b0b5ba42100
satriaprayoga <satria.prayoga@gmail.com>
2020-05-04 11:10:00 +0700

    test

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React, { Component, Fragment} from 'react';
 //import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
-import Header from '../../components/Header';
-import TabComponent from '../../components/TabComponent';
-import Footer from '../../components/Footer';
+import Header from '../../Components/Header';
+import TabComponent from '../../Components/TabComponent';
+import Footer from '../../Components/Footer';
 
 class Home extends Component {
     render() {

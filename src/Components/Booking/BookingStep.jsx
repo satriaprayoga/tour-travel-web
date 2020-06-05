e2ae6b17c129eda108624a72c3db8f008c752241
Sandy Setiawan <sndystiawan@Sandys-MacBook-Air.local>
2020-06-05 15:46:38 +0700

    test

--- src/Components/Booking/BookingStep.jsx
+++ src/Components/Booking/BookingStep.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './BookingStep.css'
 import logo from '../../assets/logobig.png'
+import BookingInfo from './Steps/BookingInfo';
 import { Link, Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import qs from 'qs';
 import * as Yup from 'yup';
